961dbc055dca5533e33cf1ac4d6cd69827527f1b
Hedgehog7777 <kvazar-7@mail.ru>
2022-04-18 18:18:44 +0900

    Homework_7

---
+++ lesson_7/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <ctime>
+#include "Blackjack.hpp"
+
+using namespace std;
+
+int main()
+{
+
+//Задание №5 ********************************************************************************
+cout << "\t\tWelcome to Blackjack!\n\n";
+
+	int numPlayers = 0;
+	while (numPlayers < 1 || numPlayers > 7)
+	{
+		cout << "How many players? (1 - 7): ";
+		cin >> numPlayers;
+	}
+
+	vector<string> names;
+	string name;
+	for (int i = 0; i < numPlayers; ++i)
+	{
+		cout << "Enter player name: ";
+		cin >> name;
+		names.push_back(name);
+	}
+	cout << endl;
+
+	//the game loop
+	Game aGame(names);
+	char again = 'y';
+	while (again != 'n' && again != 'N')
+	{
+		aGame.Play();
+		cout << "\nDo you want to play again? (Y/N): ";
+		cin >> again;
+	}
+	
+	
+}
